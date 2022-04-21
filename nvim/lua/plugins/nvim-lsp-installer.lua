b9fbcb831f584f11f6299a89b74fb9d458183dd3
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2022-04-20 22:56:34 -0300

    Adds addicional lua configs to the server

--- nvim/lua/plugins/nvim-lsp-installer.lua
+++ nvim/lua/plugins/nvim-lsp-installer.lua
@@ -4,6 +4,21 @@ local lsp_installer = require("nvim-lsp-installer")
 -- or if the server is already installed).
 lsp_installer.on_server_ready(function(server)
     local opts = {}
+    if server.name == "sumneko_lua" then
+    opts = {
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { 'vim', 'use' }
+          },
+          --workspace = {
+            -- Make the server aware of Neovim runtime files
+            --library = {[vim.fn.expand('$VIMRUNTIME/lua')] = true, [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true}
+          --}
+        }
+      }
+    }
+  end
 
     -- (optional) Customize the options passed to the server
     -- if server.name == "tsserver" then
