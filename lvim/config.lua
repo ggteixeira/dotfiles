6f1cf5f6249a3cef66d7684e84817417a7e2052b
Guilherme Teixeira <24235344+vibraniumdev@users.noreply.github.com>
2022-06-24 15:00:52 -0300

    Adds Persistence configs to LunarVim

--- lvim/config.lua
+++ lvim/config.lua
@@ -220,3 +220,12 @@ lvim.plugins = {
 --     require("nvim-treesitter.highlight").attach(0, "bash")
 --   end,
 -- })
+--   event = "BufReadPre", -- this will only start session saving when an actual file was opened
+--   module = "persistence",
+--   config = function()
+--     require("persistence").setup {
+--       dir = vim.fn.expand(vim.fn.stdpath "config" .. "/session/"),
+--       options = { "buffers", "curdir", "tabpages", "winsize" },
+--     }
+--   end,
+-- },
