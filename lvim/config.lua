18717124941bc678906ef285f61820317c487c55
Guilherme Teixeira <24235344+vibraniumdev@users.noreply.github.com>
2022-06-24 15:00:42 -0300

    Removes emmet-ls

--- lvim/config.lua
+++ lvim/config.lua
@@ -205,49 +205,7 @@ lvim.plugins = {
       }
     end,
   },
-  {
-    "aca/emmet-ls",
-    config = function()
-      local lspconfig = require("lspconfig")
-      local configs = require("lspconfig/configs")
 
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities.textDocument.completion.completionItem.snippetSupport = true
-      capabilities.textDocument.completion.completionItem.resolveSupport = {
-        properties = {
-          "documentation",
-          "detail",
-          "additionalTextEdits",
-        },
-      }
-
-      if not lspconfig.emmet_ls then
-        configs.emmet_ls = {
-          default_config = {
-            cmd = { "emmet-ls", "--stdio" },
-            filetypes = {
-              "html",
-              "svelte",
-              "vue",
-              "javascriptreact",
-              "typescriptreact",
-              "php",
-              "xml",
-              "css",
-              "less",
-              "postcss",
-              "sass",
-              "scss",
-            }, root_dir = function(fname)
-              return vim.loop.cwd()
-            end,
-            settings = {},
-          },
-        }
-      end
-      lspconfig.emmet_ls.setup({ capabilities = capabilities })
-    end,
-  },
 }
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 -- vim.api.nvim_create_autocmd("BufEnter", {
