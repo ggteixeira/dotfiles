d7563369e0cce0aa02c93dc215f861cfb35d2294
Guilherme Teixeira <24235344+vibraniumdev@users.noreply.github.com>
2023-05-27 22:19:53 -0300

    Renames nvim folder to nvim-vanilla

---
+++ nvim-vanilla/lua/plugins/nvim-cmp.lua
@@ -0,0 +1,88 @@
+-----------------------------------------------------------
+-- Autocomplete configuration file
+-----------------------------------------------------------
+
+-- Plugin: nvim-cmp
+-- url: https://github.com/hrsh7th/nvim-cmpa
+
+local tabnine = require('cmp_tabnine.config')
+tabnine:setup({
+  max_lines = 1000;
+  max_num_results = 20;
+  sort = true;
+  run_on_every_keystroke = true;
+  snippet_placeholder = '..';
+  ignored_file_types = { -- default is not to ignore
+    -- uncomment to ignore in lua:
+    -- lua = true
+  };
+  show_prediction_strength = false;
+})
+
+local cmp_status_ok, cmp = pcall(require, 'cmp')
+if not cmp_status_ok then
+  return
+end
+
+local luasnip_status_ok, luasnip = pcall(require, 'luasnip')
+if not luasnip_status_ok then
+  return
+end
+
+cmp.setup {
+  -- Load snippet support
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+
+  -- Completion settings
+  completion = {
+    --completeopt = 'menu,menuone,noselect'
+    keyword_length = 2
+  },
+
+  -- Key mapping
+  mapping = {
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+
+    -- Tab mapping
+    ['<Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end,
+    ['<S-Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end
+  },
+
+  -- Load sources, see: https://github.com/topics/nvim-cmp
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+    { name = 'path' },
+    { name = 'buffer' },
+    { name = 'cmp_tabnine' },
+  },
+}
---
+++ nvim-vanilla/lua/themes/colorschemes.lua
@@ -0,0 +1,7 @@
+ --setup must be called before loading
+
+--vim.cmd("colorscheme everforest")
+vim.cmd("colorscheme nordfox")
+--vim.cmd("colorscheme carbon")
+--vim.cmd("colorscheme duskfox")
+--vim.cmd("colorscheme onedark")
