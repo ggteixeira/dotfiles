a662d3c291d9ab194f59be236e36c63075a7cdfd
Guilherme Teixeira <24235344+vibraniumdev@users.noreply.github.com>
2023-06-16 14:37:13 -0300

    nvim | removes pathbar (redundant) and increases the whichBar timeout

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,8 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- Path bar above all tabs. Pretty useful ✨
-vim.opt.winbar = "%=%m %f"
+-- -- Path bar above all tabs. Pretty useful ✨
+-- vim.opt.winbar = "%=%m %f"
 
 -- Timeout until the which key bar appear on <Leader> press
-vim.opt.timeoutlen = 100
+vim.opt.timeoutlen = 200
