e09fd9adb4ce9ffee5822132565f459dbac833e9
ggteixeira <24235344+ggteixeira@users.noreply.github.com>
2023-10-30 14:09:13 -0300

    Adds Poimandres configs

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -45,9 +45,11 @@ return {
     priority = 1000,
     config = function()
       require("poimandres").setup({
-        -- leave this setup function empty for default config
-        -- or refer to the configuration section
-        -- for configuration options
+        bold_vert_split = false, -- use bold vertical separators
+        dim_nc_background = false, -- dim 'non-current' window backgrounds
+        disable_background = false, -- disable background
+        disable_float_background = false, -- disable background for floats
+        disable_italics = false, -- disable italics
       })
     end,
 
