5cae0ae3978879aa43fa2a6b015f07b337d0a902
ggteixeira <24235344+ggteixeira@users.noreply.github.com>
2023-10-30 19:07:52 -0300

    Fixes split (Poimandres)

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -45,7 +45,7 @@ return {
     priority = 1000,
     config = function()
       require("poimandres").setup({
-        bold_vert_split = false, -- use bold vertical separators
+        bold_vert_split = true, -- use bold vertical separators
         dim_nc_background = false, -- dim 'non-current' window backgrounds
         disable_background = false, -- disable background
         disable_float_background = false, -- disable background for floats
