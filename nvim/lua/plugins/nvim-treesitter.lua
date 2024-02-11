2e67edfa378d77b3004f04df5109e09afebbaaee
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2024-02-11 15:42:22 -0300

    Adds Elixir to the ensure_installed list (treesitter)

---
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,25 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      -- "bash",
+      -- "html",
+      -- "javascript",
+      -- "json",
+      -- "lua",
+      -- "markdown",
+      -- "markdown_inline",
+      -- "python",
+      -- "query",
+      -- "regex",
+      -- "tsx",
+      -- "typescript",
+      -- "vim",
+      -- "yaml",
+      "elixir",
+      -- "eelixir",
+      "heex",
+      "surface",
+    },
+  },
+}
