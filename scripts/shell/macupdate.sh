56ade4762b3a6ab2c017b45aed95f368c99a1696
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2024-02-13 23:46:35 -0300

    Moves Scripts folder to improve organization

---
+++ scripts/shell/macupdate.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+echo -e "Brew update"
+brew update
+
+echo -e "Brew upgrade"
+brew upgrade
+
+echo -e "Brew cleanup"
+brew cleanup
+
+echo -e "Brew doctor"
+brew doctor
+
+echo -e "OMZ update"
+omz update
