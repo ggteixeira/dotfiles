c8bc656975c4e6a910bc6fc4587eb2ed623728a5
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2024-05-17 09:48:47 -0300

    Reinstalls Lazyvim

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,7 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+-- Timeout until the which key bar appear on <Leader> press
+vim.opt.timeoutlen = 200
+vim.opt.swapfile = false
---
+++ nvim/lua/plugins/auto-dark-mode.lua
@@ -0,0 +1,14 @@
+return {
+  "f-person/auto-dark-mode.nvim",
+  config = {
+    update_interval = 1000,
+    set_dark_mode = function()
+      vim.api.nvim_set_option("background", "dark")
+      vim.cmd("colorscheme gruvbox")
+    end,
+    set_light_mode = function()
+      vim.api.nvim_set_option("background", "light")
+      vim.cmd("colorscheme gruvbox")
+    end,
+  },
+}
