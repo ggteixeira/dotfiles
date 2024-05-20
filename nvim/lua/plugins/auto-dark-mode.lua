aa0ecf40e14c61f47ae8a8dfcdafcf8322de251e
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2024-05-20 19:12:16 -0300

    Re-enables the auto-dark-mode plugin and updates its configs

--- nvim/lua/plugins/auto-dark-mode.lua
+++ nvim/lua/plugins/auto-dark-mode.lua
@@ -1,14 +1,14 @@
 return {
   "f-person/auto-dark-mode.nvim",
-  config = {
+  opts = {
     update_interval = 1000,
     set_dark_mode = function()
-      vim.api.nvim_set_option("background", "dark")
-      vim.cmd("colorscheme gruvbox")
+      vim.api.nvim_set_option_value("background", "dark", {})
+      vim.cmd("colorscheme catppuccin")
     end,
     set_light_mode = function()
-      vim.api.nvim_set_option("background", "light")
-      vim.cmd("colorscheme gruvbox")
+      vim.api.nvim_set_option_value("background", "light", {})
+      vim.cmd("colorscheme catppuccin-latte")
     end,
   },
 }
--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,5 +1,5 @@
 return {
   -- { "ggandor/leap.nvim", enabled = false },
-  { "f-person/auto-dark-mode.nvim", enabled = false },
+  -- { "f-person/auto-dark-mode.nvim", enabled = false },
   { "folke/flash.nvim", enabled = false },
 }
