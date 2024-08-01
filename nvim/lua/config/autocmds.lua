68e904198f056e8beb7dc2ca25e31fd766fec75b
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2024-08-01 20:03:51 -0300

    Disables spellchecker because I'm a linguist nessa porra

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,17 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+-- wrap and check for spell in text filetypes
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("wrap_spell"),
+  pattern = { "text", "plaintex", "typst", "gitcommit", "markdown" },
+  callback = function()
+    -- vim.opt_local.wrap = true
+    vim.opt_local.spell = false
+  end,
+})
