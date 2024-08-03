dc64cb0f9f91cba5c1f771d5e918e486c6c92dd9
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2024-08-03 14:25:13 -0300

    Theme

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -4,31 +4,12 @@ return {
   { "Mofiqul/dracula.nvim" },
   { "ellisonleao/gruvbox.nvim" },
   { "rose-pine/neovim", name = "rose-pine" },
-  { "catppuccin/nvim", name = "catppuccin" },
   { "Mofiqul/dracula.nvim" },
-  {
-    "olivercederborg/poimandres.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("poimandres").setup({
-        bold_vert_split = true, -- use bold vertical separators
-        dim_nc_background = false, -- dim 'non-current' window backgrounds
-        disable_background = false, -- disable background
-        disable_float_background = false, -- disable background for floats
-        disable_italics = false, -- disable italics
-      })
-    end,
-
-    -- optionally set the colorscheme within lazy config
-    init = function()
-      vim.cmd("colorscheme poimandres")
-    end,
-  },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   {
     "LazyVim/LazyVim",
     opts = {
-      -- colorscheme = "catppuccin-mocha",
+      colorscheme = "catppuccin-mocha",
       -- colorscheme = "catppuccin-macchiato",
       -- colorscheme = "catppuccin-frappe",
       -- colorscheme = "catppuccin-latte",
@@ -36,7 +17,7 @@ return {
       -- colorscheme = "rose-pine-main",
       -- colorscheme = "rose-pine-dawn", -- light version
       -- colorscheme = "poimandres",
-      colorscheme = "gruvbox",
+      -- colorscheme = "gruvbox",
       -- colorscheme = "dracula",
       -- colorscheme = "everforest",
       -- colorscheme = "dracula",
