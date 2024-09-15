030e065c187b39a649905c828528d11ed5fcdd1b
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2024-09-14 22:53:40 -0300

    Improves Grubbox configs

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -1,11 +1,38 @@
 return {
   { "rmehri01/onenord.nvim", branch = "main" },
   { "sainnhe/everforest" },
-  { "Mofiqul/dracula.nvim" },
-  { "ellisonleao/gruvbox.nvim" },
   { "rose-pine/neovim", name = "rose-pine" },
   { "Mofiqul/dracula.nvim" },
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { "catppuccin/nvim", lazy = true, name = "catppuccin" },
+  {
+    "ellisonleao/gruvbox.nvim",
+    lazy = true,
+    name = "gruvbox",
+    opts = {
+      terminal_colors = true, -- add neovim terminal colors
+      undercurl = true,
+      underline = true,
+      bold = true,
+      italic = {
+        strings = true,
+        emphasis = true,
+        comments = true,
+        operators = false,
+        folds = true,
+      },
+      strikethrough = true,
+      invert_selection = false,
+      invert_signs = false,
+      invert_tabline = false,
+      invert_intend_guides = false,
+      inverse = true, -- invert background for search, diffs, statuslines and errors
+      contrast = "hard", -- can be "hard", "soft" or empty string
+      palette_overrides = {},
+      overrides = {},
+      dim_inactive = false,
+      transparent_mode = false,
+    },
+  },
   {
     "LazyVim/LazyVim",
     opts = {
