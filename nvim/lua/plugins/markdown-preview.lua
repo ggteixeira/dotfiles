6a8cb1b13c2850b2a97163ea7835ab1f3573e762
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2024-09-24 22:32:53 -0300

    Disables markdown features and installs preview manually
    Bugadíssimo o LazyVim

---
+++ nvim/lua/plugins/markdown-preview.lua
@@ -0,0 +1,19 @@
+return {
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  build = function()
+    require("lazy").load({ plugins = { "markdown-preview.nvim" } })
+    vim.fn["mkdp#util#install"]()
+  end,
+  keys = {
+    {
+      "<leader>cp",
+      ft = "markdown",
+      "<cmd>MarkdownPreviewToggle<cr>",
+      desc = "Markdown Preview",
+    },
+  },
+  config = function()
+    vim.cmd([[do FileType]])
+  end,
+}
