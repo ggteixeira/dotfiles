332accd4981bb828af5fee743e3a165e957b13e7
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2024-10-06 16:12:15 -0300

    Makes Gruvbox softer (tira o vermelhão do constraste)

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -27,7 +27,7 @@ return {
       invert_tabline = false,
       invert_intend_guides = false,
       inverse = true, -- invert background for search, diffs, statuslines and errors
-      contrast = "hard", -- can be "hard", "soft" or empty string
+      contrast = "soft", -- can be "hard", "soft" or empty string
       palette_overrides = {},
       overrides = {},
       dim_inactive = false,
