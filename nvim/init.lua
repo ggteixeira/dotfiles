5b96981dd93f9281c96bea93462134bab8abb597
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2024-10-11 13:39:57 -0300

    Configures telescope to ignore node_modules

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,13 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+require("telescope").setup({
+  defaults = {
+    file_ignore_patterns = {
+      "node_modules",
+    },
+  },
+})
