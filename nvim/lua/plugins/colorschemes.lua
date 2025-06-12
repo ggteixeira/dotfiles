0d416bf399ccda2a0828a511a0637c0687dc4c00
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2025-06-11 22:46:43 -0300

    Adds themes

---
+++ nvim/lua/plugins/colorschemes.lua
@@ -0,0 +1,14 @@
+return {
+  -- Add themes
+  { "catppuccin/nvim", lazy = false, name = "catppuccin" },
+  { "ellisonleao/gruvbox.nvim" },
+  { "rmehri01/onenord.nvim", branch = "main" },
+  { "sainnhe/everforest" },
+  { "rose-pine/neovim", name = "rose-pine" },
+  { "Mofiqul/dracula.nvim" },
+
+  -- Configure LazyVim to load the chosen colorscheme
+  { "LazyVim/LazyVim", opts = {
+    colorscheme = "gruvbox",
+  } },
+}
