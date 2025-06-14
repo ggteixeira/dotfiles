e1a30cd7411ff754265072fa76a65d8973e53799
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2025-06-14 16:58:03 -0300

    Adds plugins and adds basic autocmd

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,17 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- wrap and check for spell in text filetypes
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("wrap_spell"),
+  pattern = { "text", "plaintex", "typst", "gitcommit", "markdown" },
+  callback = function()
+    -- vim.opt_local.wrap = true
+    vim.opt_local.spell = false
+  end,
+})
