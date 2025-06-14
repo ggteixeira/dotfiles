9303f019473543641ce366a89fbce2eae753d2f0
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2025-06-14 16:57:32 -0300

    Updates standard keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,18 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Resizing windows (ctrl+shift+arrows)
+vim.keymap.set("n", "<C-S-Up>", ":horizontal resize +1 <CR>", { silent = true })
+vim.keymap.set("n", "<C-S-Down>", ":horizontal resize -1 <CR>", { silent = true })
+
+vim.keymap.set("n", "<C-S-Right>", ":vertical resize +2 <CR>", { silent = true })
+vim.keymap.set("n", "<C-S-Left>", ":vertical resize -2 <CR>", { silent = true })
+
+-- Use ; to access command mode:
+vim.keymap.set("n", ";", ":")
+vim.keymap.set("v", ";", ":")
+
+-- Do the opposite to achieve the repeat functionality?
+vim.keymap.set("n", ":", ";")
+vim.keymap.set("v", ":", ";")
